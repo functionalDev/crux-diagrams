ff23fc4174040c959aab172d387a4c1be838e164
stolzp <paul.stolz@dw.com>
2024-04-20 08:07:46 +0200

    proper build and close nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,9 +5,10 @@ import styles from './Navigation.module.css';
 
 export const Navigation = () => {
     const [isOpen, setIsOpen] = createSignal(false);
+    document.addEventListener('click', (e) => !e.target.classList.contains(styles.burgerButton) && setIsOpen(false))
     return (
     <nav class={styles.nav}>
-        <div  class={styles.burgerButton} onClick={() => setIsOpen(state => !state)}>
+        <div class={styles.burgerButton} onClick={() => setIsOpen(state => !state)}>
             🍔
         </div>
         <Show when={isOpen()}>
